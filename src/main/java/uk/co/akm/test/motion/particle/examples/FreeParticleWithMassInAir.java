4a9eacc1dbb471ede638c0b49039cded6cccede9
Thanos Mavroidis <thanosmavroidis@Thanoss-MBP.home>
2019-01-05 11:35:28 +0000

    Minor change in comments.

--- src/main/java/uk/co/akm/test/motion/particle/examples/FreeParticleWithMassInAir.java
+++ src/main/java/uk/co/akm/test/motion/particle/examples/FreeParticleWithMassInAir.java
@@ -5,7 +5,7 @@ import uk.co.akm.test.motion.particle.Particle;
 /**
  * Particle of mass m moving in a straight line along the x-axis experiencing a resistance against its motion. This
  * resistance is proportional to the square of the particle speed, when that speed is high or just proportional to the
- * speed, when the speed is low. The transition speed between high and low speed values is taken to be 1 m.sec. The
+ * speed, when the speed is low. The transition speed between high and low speed values is taken to be 1 m/sec. The
  * initial particle speed is vx0, where vx0 > 1, and the initial particle position is 0.
  *
  * Created by Thanos Mavroidis on 07/01/2018.
